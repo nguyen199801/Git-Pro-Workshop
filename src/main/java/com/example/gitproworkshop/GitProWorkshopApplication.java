fb2e44c817de3acf81e88de31ef696310dd7e816
nguyen199801 <nguyennc710@gmail.com>
2021-11-02 13:31:50 +0700

    initial setup

---
+++ src/main/java/com/example/gitproworkshop/GitProWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitproworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProWorkshopApplication.class, args);
+    }
+
+}
