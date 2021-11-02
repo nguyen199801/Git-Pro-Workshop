c011ac7da8f672551c9c5e7b61088db3c646a235
vu pham <levu2909@gmail.com>
2021-11-02 14:25:51 +0700

    b2

--- src/main/java/GitProWorkshop.java
+++ src/main/java/GitProWorkshop.java
@@ -1,14 +1,16 @@
 public class GitProWorkshop {
     public static void main(String[] args) {
         greet();
+        count("sadge");
     }
 
     public static void greet() {
         System.out.println("Nguyen Nguyen");
     }
 
-    public void count() {
+    public static void count(String param) {
         System.out.println("Vu Pham");
+        System.out.println("Nem is " + param + " without Nhi");
     }
     public void cheer(){
         System.out.println("Sadge");
