15a9f71d298020eb66d52c824b9e8fc75476cffb
nguyen199801 <nguyennc710@gmail.com>
2021-11-02 13:59:34 +0700

    a1

--- src/main/java/GitProWorkshop.java
+++ src/main/java/GitProWorkshop.java
@@ -1,4 +1,9 @@
 public class GitProWorkshop {
     public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Nguyen Nguyen");
     }
 }
